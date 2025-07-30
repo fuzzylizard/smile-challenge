cf485a9fab8c3f461c4733a30e33d1e945efc832
Christopher Johnston <chris@fuzzylizard.com>
2025-07-30 14:49:42 -0400

    initial integration with smile.io, gets points for single user

--- server/app/controllers/points_controller.rb
+++ server/app/controllers/points_controller.rb
@@ -1,5 +1,28 @@
+require "uri"
+require "json"
+require "net/http"
+
 class PointsController < ApplicationController
   def index
-    render json: {points: 27500}, status: :ok
+    points = get_points
+
+    render json: { points: points }, status: :ok
+  end
+
+  private
+
+  def get_points
+    url = URI("https://api.smile.io/v1/customers/3012968197")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "Bearer #{ENV["API_KEY"]}"
+    request["Content-Type"] = "application/json"
+
+    response = https.request(request)
+    body = JSON.parse(response.body)
+    body["customer"]["points_balance"]
   end
 end
