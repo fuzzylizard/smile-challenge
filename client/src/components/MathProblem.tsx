759c9368c5d358fdf9747ca742323a8fe408e9c7
Christopher Johnston <chris@fuzzylizard.com>
2025-07-30 21:03:48 -0400

    Has ability to create new Activity (Math Problem)
    - refactored smile code into separate class

--- client/src/components/MathProblem.tsx
+++ client/src/components/MathProblem.tsx
@@ -1,9 +1,26 @@
+import {useState} from "react";
+
 function MathProblem() {
+    const [solution, setSolution] = useState("")
+
+    function handleSubmitMathProblem() {
+        console.log("Submit...");
+        
+    }
+
     return (
-        <>
-            <div>5 + 5 = </div>
-            <input type="text"/>
-        </>
+        <div>
+            <p>For 100 Reward Points, Solve The Following</p>
+            5 + 5 =
+            <input
+                type="text"
+                value={solution}
+                onChange={(e) => {
+                    setSolution(e.target.value)
+                }}
+                className="border-1 p-2 rounded-lg m-2 w-32 border-neutral-600"/>
+            <button onClick={handleSubmitMathProblem}>Submit</button>
+        </div>
     );
 }
 
--- server/app/controllers/points_controller.rb
+++ server/app/controllers/points_controller.rb
@@ -1,28 +1,11 @@
-require "uri"
-require "json"
-require "net/http"
-
 class PointsController < ApplicationController
   def index
-    points = get_points
+    points = LoyaltyProgram.new.get_points(3012968197)
 
     render json: { points: points }, status: :ok
   end
 
-  private
-
-  def get_points
-    url = URI("https://api.smile.io/v1/customers/3012968197")
-
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-    request["Authorization"] = "Bearer #{ENV["API_KEY"]}"
-    request["Content-Type"] = "application/json"
-
-    response = https.request(request)
-    body = JSON.parse(response.body)
-    body["customer"]["points_balance"]
+  def create
+    
   end
 end
